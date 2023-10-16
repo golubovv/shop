b0fcb3401df62c92f1b25fbaf434adc2f7ba2529
Koninski <139650678+Koninski@users.noreply.github.com>
2023-10-17 02:36:10 +0300

    Заготовка
    На данном этапе Проверить модели в админке

--- main/views.py
+++ main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from .model import goods
+from .models import *
 from django.views.generic import ListView
 
 
 class GoodsAll(ListView):
-  model = goods
-  template_name = 'main/goodsall.html'
+  pass
