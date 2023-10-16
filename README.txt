9d42e6eba851b7cff49862ba2c0b6647bd0df1da
Koninski <139650678+Koninski@users.noreply.github.com>
2023-10-17 02:17:14 +0300

    Добавлены модели
    Изменен Redmi.txt
    Дополнительно для работы необходим пакет Pillow
    
    Добавлены модели согласно структуре БД.
    - Buyers
    - Orders
    - Goods
    - PropertyGoods
    - Makers
    
    Добавлено отображение моделей в "админке"
    - Buyers
    - Orders
    - Goods
    - PropertyGoods
    - Makers
    
    В settings.py добавлена папка для загрузок медиаданных - media,
    Убрал ПОРТ в настройках подключения к БД
    
    в .gitignore добавлена папка media

--- README.txt
+++ README.txt
@@ -3,6 +3,7 @@
 Django 3.2          //// команда pip install Django==3.2
 python-dotenv       //// команда pip install python-dotenv
 psycopg2            //// команда pip install psycopg2
+Pillow              //// команда pip install Pillow
 
 
 В директории shop/shopet создать файл .env, в этом файле указать приватные переменные
@@ -12,9 +13,10 @@ psycopg2            //// команда pip install psycopg2
 SECRET_KEY = %cie*mik8m%*_z=#r-%$)9ji#=$ztfft06jk@j4f74i50*z)cf         //// Секретный ключ ......
 DEBUG = True                                                            //// Режим отладки Да/Нет
 
+ENGINE = django.db.backends.postgresql
 DATABASE = MyNameDataBase       //// Название базы данных
 USER = NameUser                 //// Имя пользователя для авторизации в БД
 PASSWORD = UserPassword         //// Пароль для пользователя в БД
 DB_HOST = localhost             //// .... Хост по умолчанию localhost
-DB_PORT = 8000                  //// .... Порт по умолчанию 8000
+DB_PORT = 8000                  //// .... Порт по умолчанию 8000, но если не пойдет что-то, то можно удалить
 ----------------------  Конец файл .env -----------------------------
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import *
+
+
+class Buyers_Admin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'numbersTel', 'shopping_cart')
+    list_display_links = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'numbersTel')
+
+
+class Orders_Admin(admin.ModelAdmin):
+    list_display = ('orders_buy', 'order_numb', 'order_date', 'status_order')
+    search_fields = ('orders_buy', 'order_date', 'order_numb')
+    list_editable = ('status_order',)
+    list_display_links = ('orders_buy', 'order_numb')
+
+
+class Goods_Admin(admin.ModelAdmin):
+    list_display = ('goods_name', 'brend', 'shop_price', 'count_goods')
+    list_display_links = ('goods_name',)
+    search_fields = ('goods_name', 'brend')
+    list_editable = ('shop_price',)
+    list_filter = ('goods_name', 'brend')
+
+
+class PropertyGoods_Admin(admin.ModelAdmin):
+    list_display = ('nameProperty', 'unitProperty', 'quantyProperty', 'boolProperty', 'boolValueProperty')
+    list_display_links = ('nameProperty',)
+    search_fields = ('nameProperty',)
+    list_editable = ('quantyProperty', 'boolProperty', 'boolValueProperty',)
+
+admin.site.register(Goods, Goods_Admin)
+admin.site.register(Orders, Orders_Admin)
+admin.site.register(Buyers, Buyers_Admin)
+admin.site.register(PropertyGoods, PropertyGoods_Admin)
+admin.site.register(Categories)
+admin.site.register(Makers)
 
 # Register your models here.
